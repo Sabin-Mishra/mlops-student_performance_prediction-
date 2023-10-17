38716e280bef6a0d7fa5cc8c5271413647da9ba1
Sabin Mishra <sabin@parsedom.com>
2023-10-17 21:51:24 +0545

    data transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -32,9 +33,9 @@ class DataIngestion:
 
             train_set,test_set = train_test_split(df,test_size=0.2, random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)       # save the train dataset csv 
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)       # save the train dataset to csv 
 
-            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)         # save the test dataset csv
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)         # save the test dataset to csv
 
             logging.info("Ingestion of the data is completed")
             return(
@@ -47,7 +48,10 @@ class DataIngestion:
             
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
 
 
